2539f4d16ca1440cbcafd05ce23378b3e712c8b7
AstralxOilx <www.survival5678@gmail.com>
2025-04-29 05:39:43 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,13 +8,13 @@ import {
 const isPublicPage = createRouteMatcher(["/auth"])
 
 export default convexAuthNextjsMiddleware(async (request) => {
-  // if (!isPublicPage(request) && !(await isAuthenticatedNextjs())) {
-  //   return nextjsMiddlewareRedirect(request, "/auth");
-  // }
+  if (!isPublicPage(request) && !(await isAuthenticatedNextjs())) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
 
-  // if (isPublicPage(request) && (await isAuthenticatedNextjs())) {
-  //   return nextjsMiddlewareRedirect(request, "/")
-  // }
+  if (isPublicPage(request) && (await isAuthenticatedNextjs())) {
+    return nextjsMiddlewareRedirect(request, "/")
+  }
 });
 export const config = {
   // The following matcher runs middleware on all routes
