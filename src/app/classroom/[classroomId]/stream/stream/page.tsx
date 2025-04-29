6b5b3664f24cb0115536211a7381eec884d64177
AstralxOilx <www.survival5678@gmail.com>
2025-04-29 13:38:18 +0700

    jisit for Vercel compatibility

--- src/app/classroom/[classroomId]/stream/stream/page.tsx
+++ src/app/classroom/[classroomId]/stream/stream/page.tsx
@@ -1,29 +1,44 @@
 "use client"
 
-import { MediaRoom } from "@/features/stream/components/media-room";
-import { useClassroomId } from "@/hooks/use-classroom-id";
-import { LoaderCircle } from "lucide-react";
+import { useCurrentUser } from '@/features/auth/api/use-current-user';
+import { useCurrentMember } from '@/features/members/api/use-current-member';
+import { useClassroomId } from '@/hooks/use-classroom-id';
+// import JitsiMeet from '@/features/stream/components/jitsi-meet' 
+import { LoaderCircle } from 'lucide-react';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
 
-const StreamIdPage = () => {
-    const classroomId = useClassroomId();
+const JitsiMeet = dynamic(() => import('@/features/stream/components/jitsi-meet'), { ssr: false });
 
-    if (!classroomId) {
+
+function CreateStreamPage() {
+
+    const router = useRouter();
+    const workspaceId = useClassroomId();
+
+    
+        const { data: user, isLoading: userLoading } = useCurrentUser();
+ 
+
+    if (!user || !workspaceId || userLoading) {
         return (
-            <div className="h-screen flex justify-center items-center">
+            <div className="h-full flex-1 flex justify-center items-center flex-col gap-2 ">
                 <LoaderCircle className="size-6 animate-spin text-muted-foreground" />
             </div>
-        )
+        );
     }
 
-    return (
-        <>
 
-            <MediaRoom
-                chatId={classroomId + ''}
-                video={false}
-                audio={false}
-            />
-        </>
-    )
+    const handleRoomLink = (link: string) => {
+        // console.log("Room link: ", link);
+    };
+
+    return (
+        <div>
+            <JitsiMeet displayName={workspaceId} onRoomLinkGenerated={handleRoomLink} />
+        </div>
+    );
 }
-export default StreamIdPage;
+
+export default CreateStreamPage
